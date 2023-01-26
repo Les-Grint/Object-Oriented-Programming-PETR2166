81c1cd50e8fa20af36ec39559fec75f5f335c5f8
Les-Grint <118374812+Les-Grint@users.noreply.github.com>
2023-01-26 12:50:15 +0000

    Update User.java

--- main/java/com/example/exeriversports/User.java
+++ main/java/com/example/exeriversports/User.java
@@ -1,16 +1,22 @@
 package com.example.exeriversports;
 
+// This declares the class 'User'
 public class User {
+
+    // This declares the String variable 'Email'
     String Email;
 
+    // This declares the callable 'User' method which sets the value of the users email as the alternate variable 'Email'.
     public User(String Email){
         this.Email = Email;
     }
 
+    // This declares the callable 'getEmail' method which returns the value of the users email.
     public String getEmail() {
         return Email;
     }
 
+    // This declares the callable 'setEmail' method which sets the value of the users email.
     public void setEmail(String Email) {
         this.Email = Email;
     }
