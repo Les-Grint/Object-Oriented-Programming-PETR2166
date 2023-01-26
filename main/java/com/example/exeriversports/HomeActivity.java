417ff50bb527785cf9afd1ae0208a877c6df9d3b
Les-Grint <118374812+Les-Grint@users.noreply.github.com>
2023-01-26 12:46:20 +0000

    Update HomeActivity.java

--- main/java/com/example/exeriversports/HomeActivity.java
+++ main/java/com/example/exeriversports/HomeActivity.java
@@ -1,22 +1,19 @@
 package com.example.exeriversports;
-
 import androidx.annotation.NonNull;
 import  androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 
 public class HomeActivity extends AppCompatActivity {
 
+    // This declares variable for the Logout Button.
     Button Logout;
 
+    // This declares variable for the Navigation bar.
     BottomNavigationView bottomNavigationView;
 
     @Override
@@ -24,24 +21,26 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // This initialises the variable and assigns the values of the button to it.
+        Logout = findViewById(R.id.btn_logout);
 
-
-        Logout = (Button) findViewById(R.id.btn_logout);
-
+        // This is the listener for the 'Logout' button.
         Logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             // This will remove all values saved in session and redirect to the login screen
             SessionManagement sessionManagement = new SessionManagement(HomeActivity.this);
             sessionManagement.removeSession();
+            // This will navigate to the Login Activity page.
             Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(intent);
             }
         });
 
-                bottomNavigationView = findViewById(R.id.bottom_navigator);
+        // This initialises a variable and assigns the values of the navigation bar to it to.
+        bottomNavigationView = findViewById(R.id.bottom_navigator);
+        // This sets the events item in the navigation bar to 'selected'
         bottomNavigationView.setSelectedItemId(R.id.Home);
-
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -52,17 +51,26 @@ public class HomeActivity extends AppCompatActivity {
                         return true;
 
                     case R.id.Profile:
+                        // This navigates to the Profile Activity page
                         startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
                         overridePendingTransition(0,0);
                         return true;
 
                     case R.id.Events:
+                        // This navigates to the Events Activity page
                         startActivity(new Intent(getApplicationContext(), EventsActivity.class));
                         overridePendingTransition(0,0);
                         return true;
 
                     case R.id.About:
+                        // This navigates to the About Activity page
                         startActivity(new Intent(getApplicationContext(), AboutActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.Weather:
+                        // This navigates to the Weather Activity page
+                        startActivity(new Intent(getApplicationContext(), WeatherActivity.class));
                         overridePendingTransition(0,0);
                         return true;
                 }
