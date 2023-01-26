90aef39ddc59982721fcfc89e08db093c5ab078c
Les-Grint <118374812+Les-Grint@users.noreply.github.com>
2023-01-26 12:31:34 +0000

    Update AboutActivity.java

--- main/java/com/example/exeriversports/AboutActivity.java
+++ main/java/com/example/exeriversports/AboutActivity.java
@@ -11,15 +11,18 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AboutActivity extends AppCompatActivity {
 
+    // This declares variable for the Navigation bar.
     BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        bottomNavigationView = findViewById(R.id.bottom_navigator);
-        bottomNavigationView.setSelectedItemId(R.id.About);
 
+        // This initialises a variable and assigns the values of the navigation bar to it to.
+        bottomNavigationView = findViewById(R.id.bottom_navigator);
+        // This sets the events item in the navigation bar to 'selected'
+        bottomNavigationView.setSelectedItemId(R.id.About);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -27,21 +30,30 @@ public class AboutActivity extends AppCompatActivity {
                 switch (item.getItemId())
                 {
                     case R.id.Home:
+                        // This navigates to the Home Activity page
                         startActivity(new Intent(getApplicationContext(), HomeActivity.class));
                         overridePendingTransition(0,0);
                         return true;
 
                     case R.id.Profile:
+                        // This navigates to the Profile Activity page
                         startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
                         overridePendingTransition(0,0);
                         return true;
 
                     case R.id.Events:
+                        // This navigates to the Events Activity page
                         startActivity(new Intent(getApplicationContext(), EventsActivity.class));
                         overridePendingTransition(0,0);
                         return true;
 
                     case R.id.About:
+                        return true;
+
+                    case R.id.Weather:
+                        // This navigates to the Weather Activity page
+                        startActivity(new Intent(getApplicationContext(), WeatherActivity.class));
+                        overridePendingTransition(0,0);
                         return true;
                 }
                 return false;
